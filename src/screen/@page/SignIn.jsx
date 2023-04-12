f20082bc04ba16b472f8aae8e40a6345aca25ac7
1 <wrasf175@naver.com>
2023-04-12 18:34:32 +0900

    style: signIn 페이지 html 마크업 구현

--- src/screen/@page/SignIn.jsx
+++ src/screen/@page/SignIn.jsx
@@ -1,7 +1,61 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <form style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    }}>
+    이메일
+    <input
+      data-testid="email-input"
+      placeholder='이메일을 입력해주세요'
+      onChange={()=>{}}
+      style= {{
+        marginTop: '1rem',
+        marginBottom: '1rem',
+        color: 'white',
+        width: '12rem',
+    }}
+    />
+    비밀번호
+    <input
+      type="password"
+      data-testid="password-input"
+      placeholder='8자 이상'
+      onChange={()=>{}}
+      style= {{
+        marginTop: '1rem',
+        color: 'white',
+        width: '12rem',
+    }}
+    />
+    <button
+      data-testid="signin-button"
+      onClick={()=>{}}
+      style= {{
+        backgroundColor: 'black',
+        marginTop: '1rem',
+        color: 'white',
+        width: '12rem',
+    }}
+    >
+      로그인
+    </button>
+    <Link to='/SignUp' 
+        style= {{
+            marginTop: '1rem',
+            color: 'black',
+            textDecoration: 'none',
+            border: '1px solid black',
+            borderRadius: '5px',
+            width: '8rem',
+            textAlign: 'center',
+        }}
+    >
+        회원가입</Link>
+  </form>
   )
 }
