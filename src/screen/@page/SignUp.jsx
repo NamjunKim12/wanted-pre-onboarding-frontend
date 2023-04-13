2b3f67cc1df90411738ef06f43f111827853ce4d
1 <wrasf175@naver.com>
2023-04-13 19:22:12 +0900

    style: signUp 페이지 html 마크업 구현

--- src/screen/@page/SignUp.jsx
+++ src/screen/@page/SignUp.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <form style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    }}>
+    이메일
+    <input
+      data-testid="email-input"
+      placeholder='이메일을 입력해주세요'
+      onChange={()=>{}}
+      style= {{
+        marginTop: '1rem',
+        marginBottom: '1rem',
+        color: 'white',
+        width: '12rem',
+    }}
+    />
+    비밀번호
+    <input
+      type="password"
+      data-testid="password-input"
+      placeholder='8자 이상 입력해주세요.'
+      onChange={()=>{}}
+      style= {{
+        marginTop: '1rem',
+        color: 'white',
+        width: '12rem',
+    }}
+    />
+    <button
+      data-testid="signup-button"
+      onClick={()=>{}}
+      style= {{
+        backgroundColor: 'black',
+        marginTop: '1rem',
+        color: 'white',
+        width: '12rem',
+    }}
+    >
+      회원가입
+    </button>
+  </form>
   )
 }
